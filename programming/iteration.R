2d03741e2be63c17c98979c7b7d10f1365796e94
isabelyb <isabelcristinayepesb@gmail.com>
2021-04-20 16:36:33 -0500

    r4ds 2021-04-20 16:36

--- programming/iteration.R
+++ programming/iteration.R
@@ -10,3 +10,31 @@ df <- tibble(
 
 median(df$a)
 
+# output <- vector("double", length(x))
+output <- vector("double", ncol(df))
+for (i in seq_along(df)) {
+  output[[i]] <- median(df[[i]])
+}
+output
+
+
+#
+
+out <- ""
+for (x in letters) {
+  out <- stringr::str_c(out, x)
+}
+
+x <- sample(100)
+sd <- 0
+for (i in seq_along(x)) {
+  sd <- sd + (x[i] - mean(x)) ^ 2
+}
+sd <- sqrt(sd / (length(x) - 1))
+
+x <- runif(100)
+out <- vector("numeric", length(x))
+out[1] <- x[1]
+for (i in 2:length(x)) {
+  out[i] <- out[i - 1] + x[i]
+}
